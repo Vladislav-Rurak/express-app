b05d61762d1e59c814d8736a56cff99f9388005d
bbd <dolgkozly@gmail.com>
2022-10-27 12:22:08 +0300

    Add server

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const http = require('http');
 const express = require('express');
-const { response } = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).end();
 });
+
+app.post('/', (req, res) => {});
+app.patch('/', (req, res) => {});
+app.delete('/', (req, res) => {});
+
+app.get('/contest', (req, res) => {});
+app.post('/contest', (req, res) => {});
+app.patch('/contest/1', (req, res) => {});
+app.delete('/contest/1', (req, res) => {});
 
 const PORT = process.env.PORT || 5000;
 
